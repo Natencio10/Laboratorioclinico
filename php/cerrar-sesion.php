46ebd68b9460152b7041840af61f489f2b7b6849
Natencio10 <84600578+Natencio10@users.noreply.github.com>
2021-12-08 22:32:31 -0400

    Delete php directory

--- php/cerrar-sesion.php
+++
@@ -1,7 +0,0 @@
-<?php
- session_start();
- $_SESSION = array();
- session_destroy();
-echo '<script type="text/javascript">alert("Adios");</script>';
-echo "<script>window.location = '../index.php'</script>";
-?>
